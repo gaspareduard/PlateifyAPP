fc574eaf32ab9926ad7a3532ce1b4cbf8cf91b07
Eduard Gaspar <gaspareduard@yahoo.com>
2025-03-28 04:16:17 +0000

    Initial Commit

---
+++ Plateify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plateify
+//
+//  Created by Eduard Gaspar on 28.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plateify/PlateifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateifyApp.swift
+//  Plateify
+//
+//  Created by Eduard Gaspar on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct PlateifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
